4ed960e2adca9009449912847d956cde6b2ed4fc
ninja58yt <75378326+ninja58yt@users.noreply.github.com>
2024-08-02 19:18:34 -0500

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,8 @@
+let noti1 = document.getElementById('noti1')
+
+function showToast(){
+  let noti = document.createElement('div');
+  noti.classList.add('noti');
+  noti.innerHTML ='success';
+  noti.appendChild()
+}
